6d7ea2242a3996642c052a1cd5fbd35d0b8dbcc0
Arram Mandel <airum82@gmail.com>
2018-07-19 12:52:22 -0600

    Created react redux router boilerplate

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App/App';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import { createStore } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));

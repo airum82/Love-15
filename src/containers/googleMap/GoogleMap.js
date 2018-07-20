b24639106463f9a23ff5854e08fca261f97aed5b
Arram Mandel <airum82@gmail.com>
2018-07-20 13:03:18 -0600

    Added working fetch for tennis from googlemap npm

--- src/containers/googleMap/GoogleMap.js
+++ src/containers/googleMap/GoogleMap.js
@@ -3,9 +3,25 @@ import React, { Component } from 'react';
 import { mapsKey } from '../../APIkey';
 
 export class MapContainer extends Component {
+
+  fetchPlaces(mapProps, map) {
+    const { google } = mapProps;
+    const service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: { lat: 39.7392, lng: -104.9903 },
+      radius: 17000,
+      keyword: ['tennis']
+    }, (result) => {
+      console.log(result);
+    })
+
+  }
   render() {
     return (
-      <Map google={this.props.google} zoom={14} />
+      <Map 
+        google={this.props.google} 
+        zoom={14} 
+        onReady={this.fetchPlaces}/>
     )
   }
  }
